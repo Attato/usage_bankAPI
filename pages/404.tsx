46a289e67bbe3505355d7df6a9c611fcfe9c5df3
KoffeMeow <guyden999@mail.ru>
2023-01-19 00:21:59 +0300

    404 page update

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,14 +2,23 @@ import type { NextPage } from 'next';
 
 import Head from 'next/head';
 import Link from 'next/link';
+import Image from 'next/image';
 
 import styles from './404.module.scss';
 
 const ErrorPage: NextPage = () => {
 	return (
 		<div className={styles.error}>
+			<div className={styles.error_number}>
+				<Image
+					src="/page_404/404.png"
+					width={250}
+					height={250}
+					alt="error rabbit"
+				></Image>
+			</div>
 			<h2>
-				Ау! Как ты сюда попал? <br /> Тебя тут быть не должно.
+				Эй? Как ты сюда попал? <br /> Тебя тут быть не должно.
 			</h2>
 			<Link href="/">Выпинывайся.</Link>
 		</div>
